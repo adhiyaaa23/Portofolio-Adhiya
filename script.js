dffcb1632efeb649283787e6286b2feacd647d79
adhiyaaa23 <adhiyahafiz@gmail.com>
2026-05-19 21:12:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// Memilih semua link navigasi yang mengarah ke ID di halaman yang sama
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // Mencegah lompatan standar browser
-        
-        // Melakukan scroll halus ke bagian yang dituju
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('mobile-menu');
+    const navLinks = document.getElementById('nav-links');
+
+    // Menambahkan event klik pada ikon hamburger
+    menuToggle.addEventListener('click', function() {
+        // Mengubah ikon menjadi X
+        menuToggle.classList.toggle('is-active');
+        // Menampilkan atau menyembunyikan menu dari kiri
+        navLinks.classList.toggle('active');
     });
 });
